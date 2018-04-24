6275873a96c68a0f2f9fe771fd0f484291813744
Naushad Madakiya <naushad.madakiya@gmail.com>
2018-04-24 08:36:41 +0530

    Navigate to the new screen
    - use selected names to display another screen with help of navigator

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,12 @@ class RandomWordsState extends State<RandomWords> {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.list),
+            onPressed: _pushSaved,
+          )
+        ],
       ),
       body: _buildSuggestions(),
     );
@@ -82,5 +88,34 @@ class RandomWordsState extends State<RandomWords> {
         });
       },
     );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      final tiles = _saved.map((pair) {
+        return new ListTile(
+          title: new Text(
+            pair.asPascalCase,
+            style: _biggerFonts,
+          ),
+        );
+      });
+
+      final devided = ListTile
+          .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+          .toList();
+
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Saved Suggestions"),
+        ),
+        body: new ListView(
+          children: devided,
+        ),
+      );
+    }));
   }
 }
